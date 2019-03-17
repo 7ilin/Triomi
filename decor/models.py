2ad96915d44f98ca1d683a268a3d4d7e23d9cbb2
7ilin <karatvova@gmail.com>
2019-03-17 21:50:07 +0200

    add model Order

--- decor/models.py
+++ decor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Order(models.Model):
+    name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=12)
+    created_date = models.DateTimeField(default=timezone.now)
+    comment = models.TextField()
